d886075e5c7aa74fb2f5a83e62d106cc73ed524a
Jonathan Ventura <jonathanlventura3@gmail.com>
2024-04-19 21:48:48 -0300

    ajustando jsonserver

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({
-  baseURL: "https://organize-jsonserver.vercel.app",
+  baseURL: "https://organize-jsonserver.vercel.app/",
 });
 
 export default http;
